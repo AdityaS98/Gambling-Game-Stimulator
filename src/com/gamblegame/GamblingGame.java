dc866de4b18eec8d525acf2cc25c6d325dcc4aa9
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-27 16:27:25 +0530

    Added : A Programme for resiging the 50% amount of the user

--- src/com/gamblegame/GamblingGame.java
+++ src/com/gamblegame/GamblingGame.java
@@ -7,25 +7,28 @@ public class GamblingGame {
         /*
          * Initialized total amount and bet amount
          */
-
         int totalAmount = 100;
         int totalBet = 1;
+
         /*
          * If conditions for checking whether player won the game or lost the game.
          * Also calculating the result using random variable.
          */
 
-        int resultOfBet = (int) Math.floor(Math.random() * 2);
 
-        if (resultOfBet == 1) {
-            totalAmount += totalBet;
-            System.out.println("Gamble won the game");
-        } else {
-            totalAmount -= totalBet;
-            System.out.println("Gambler lost the game");
+
+            int resultOfBet = (int) Math.floor(Math.random() * 2);
+
+            if (resultOfBet == 1) {
+                totalAmount += totalBet;
+                System.out.println("Gambler won the game");
+            } else {
+                totalAmount -= totalBet;
+                System.out.println("Gambler lost the game");
+            }
+            totalAmount = totalAmount/2;
+        System.out.println("Amount that is resigned is "+totalAmount);
         }
-
     }
 
 
-}
