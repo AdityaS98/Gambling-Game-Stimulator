c7bee1d556aae81161a72c5e2f1fa0710ac540d2
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-27 16:14:15 +0530

    Added : A Programme for starting the game and checked whether player won or lost

--- src/com/gamblegame/GamblingGame.java
+++ src/com/gamblegame/GamblingGame.java
@@ -2,11 +2,30 @@ package com.gamblegame;
 
 public class GamblingGame {
 
-    /*
-     * Initialized total amount and bet amount
-     */
-    int totalAmount = 100;
-    int totalBet = 1;
+    public static void main(String args[]) {
+
+        /*
+         * Initialized total amount and bet amount
+         */
+
+        int totalAmount = 100;
+        int totalBet = 1;
+        /*
+         * If conditions for checking whether player won the game or lost the game.
+         * Also calculating the result using random variable.
+         */
+
+        int resultOfBet = (int) Math.floor(Math.random() * 2);
+
+        if (resultOfBet == 1) {
+            totalAmount += totalBet;
+            System.out.println("Gamble won the game");
+        } else {
+            totalAmount -= totalBet;
+            System.out.println("Gambler lost the game");
+        }
+
+    }
 
 
 }
