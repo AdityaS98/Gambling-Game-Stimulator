a3bb6c768654148e9167e7a667c255e5e7718337
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-27 16:47:25 +0530

    Added : A Programme for checking monthly bets played by the gambler

--- src/com/gamblegame/GamblingGame.java
+++ src/com/gamblegame/GamblingGame.java
@@ -8,27 +8,36 @@ public class GamblingGame {
          * Initialized total amount and bet amount
          */
         int totalAmount = 100;
-        int totalBet = 1;
+        int totalBet = 1, lowLimitAmount = 10, highLimitAmount = 100, lossAmount = 0, winAmount = 0;
 
         /*
          * If conditions for checking whether player won the game or lost the game.
          * Also calculating the result using random variable.
          */
 
+        for (int i = 1; i < 21; i++) {
 
 
             int resultOfBet = (int) Math.floor(Math.random() * 2);
 
             if (resultOfBet == 1) {
                 totalAmount += totalBet;
-                System.out.println("Gambler won the game");
+                winAmount++;
+
             } else {
                 totalAmount -= totalBet;
-                System.out.println("Gambler lost the game");
+                lossAmount++;
+
             }
-            totalAmount = totalAmount/2;
-        System.out.println("Amount that is resigned is "+totalAmount);
+
+
         }
+        // Display messages
+        System.out.println("Amount won by the gambler " + winAmount);
+        System.out.println("Amount that is lost by gambler " + lossAmount);
     }
+
+}
+
 
 
