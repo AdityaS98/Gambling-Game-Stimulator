9c90094779772273c93f62e1d10ba23a4811a7ec
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-27 17:24:23 +0530

    Added : A Programme for calculating the game on monthly basis

--- src/com/gamblegame/GamblingGame.java
+++ src/com/gamblegame/GamblingGame.java
@@ -8,13 +8,15 @@ public class GamblingGame {
          * Initialized total amount and bet amount
          */
         int totalAmount = 100;
-        int totalBet = 1;
+        int totalBet = 1, lossAmountBet = 0, winAmountBet = 0;
+        int winningday = 0, lossday = 0;
 
         /*
          * If conditions for checking whether player won the game or lost the game.
          * Also calculating the result using random variable.
          */
 
+        for (int i = 1; i < 21; i++) {
 
 
             int resultOfBet = (int) Math.floor(Math.random() * 2);
@@ -26,9 +28,20 @@ public class GamblingGame {
                 totalAmount -= totalBet;
                 System.out.println("Gambler lost the game");
             }
-            totalAmount = totalAmount/2;
-        System.out.println("Amount that is resigned is "+totalAmount);
+
         }
+        if (winAmountBet > lossAmountBet) {
+            winningday++;
+        } else {
+            lossday++;
+        }
+        System.out.println("Winning days in the month are " + winningday);
+        System.out.println("Losing days in the month are " + lossday);
+
     }
+
+}
+
+
 
 
